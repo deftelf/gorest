949f23caca232162a752b127b8b88fe0dc004f26
deftelf <carl@deftelf.co.uk>
2026-03-08 14:15:16 +0000

    chore: Remove unnecessary

--- composeApp/src/commonMain/kotlin/uk/co/deftelf/gorest/App.kt
+++ composeApp/src/commonMain/kotlin/uk/co/deftelf/gorest/App.kt
@@ -1,15 +1,12 @@
 package uk.co.deftelf.gorest
 
 import androidx.compose.runtime.Composable
-import org.koin.compose.KoinContext
 import uk.co.deftelf.gorest.ui.navigation.AppNavigation
 import uk.co.deftelf.gorest.ui.theme.GoRestTheme
 
 @Composable
 fun App() {
-    KoinContext {
-        GoRestTheme {
-            AppNavigation()
-        }
+    GoRestTheme {
+        AppNavigation()
     }
 }
